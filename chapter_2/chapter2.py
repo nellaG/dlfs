f6b79c004b5b67892b8ee6f1b64a87e24a9ee9bf
nellaG <seirios0107@gmail.com>
2017-03-06 02:00:44 +0900

    implements other gates such as NAND, OR

--- chapter_2/chapter2.py
+++ chapter_2/chapter2.py
@@ -21,13 +21,61 @@ def _and(x1: int, x2: int) -> int:
     return int(tmp > theta)
 
 
-def main(argv):
-    if argv == 'and':
-        for x, y in itertools.product([0, 1], [0, 1]):
-            print("AND({}, {}) = {}".format(x, y, _and(x, y)))
-    else:
-        print("No Implementation")
+def _and_improved(x1: int, x2: int) -> int:
+    '''
+    create AND gate with bias and weight.
+    '''
+    x = np.array([x1, x2])
+    weight = np.array([0.5, 0.5])
+    # -theta goes to bias
+    bias = -0.7
+
+    tmp = np.sum(weight * x) + bias
+
+    return int(tmp > 0)
+
+
+def nand(x1: int, x2: int) -> int:
+    '''
+    create NAND gate using the perceptron.
+    '''
+    x = np.array([x1, x2])
+    weight = np.array([0.5, 0.5])
+    # -theta goes to bias
+    bias = 0.7
+
+    tmp = np.sum(weight * x) + bias
+
+    return int(tmp > 0)
+
+
+def _or(x1: int, x2: int) -> int:
+    '''
+    create NAND gate using the perceptron.
+    '''
+    x = np.array([x1, x2])
+    weight = np.array([0.5, 0.5])  # only the weight and the bias are different compare to AND gate.  # noqa
+    # -theta goes to bias
+    bias = -0.2
+
+    tmp = np.sum(weight * x) + bias
+
+    return int(tmp > 0)
+
+
+def main():
+    for x, y in itertools.product([0, 1], [0, 1]):
+        print("AND({}, {}) = {}".format(x, y, _and(x, y)))
+
+    for x, y in itertools.product([0, 1], [0, 1]):
+        print("AND_IMPROVED({}, {}) = {}".format(x, y, _and_improved(x, y)))
+
+    for x, y in itertools.product([0, 1], [0, 1]):
+        print("NAND({}, {}) = {}".format(x, y, nand(x, y)))
+
+    for x, y in itertools.product([0, 1], [0, 1]):
+        print("OR({}, {}) = {}".format(x, y, _or(x, y)))
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
